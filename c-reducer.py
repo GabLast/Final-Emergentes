2cc230f37843492a79aca480e747fc4075b501b7
Gabriel <20170388@ce.pucmm.edu.do>
2021-07-29 23:55:32 -0400

    test

--- c-reducer.py
+++ c-reducer.py
@@ -1,51 +1,95 @@
 #!/usr/bin/env python
 
+# import sys
+#
+# mesActual = None
+# montoActual = 0
+# mesNext = None
+#
+# mesMayor = None
+# montoMesMayor = 0
+# mesMenor = None
+# montoMesMenor = 0
+# flag = False
+#
+# for line in sys.stdin:
+#     # Mes con Mayor y Menor Venta
+#     line = line.strip()
+#     mesNext, monto = line.split(" ")
+#
+#     try:
+#         monto = int(monto)
+#     except ValueError:
+#         continue
+#
+#     if mesActual == mesNext:
+#         montoActual += monto
+#     else:
+#         if mesActual:
+#             if not flag:
+#                 montoMesMenor = montoActual
+#                 montoMesMayor = montoActual
+#
+#                 mesMayor = mesActual
+#                 mesMenor = mesActual
+#
+#                 flag = True
+#             elif montoActual > montoMesMayor:
+#                 montoMesMayor = montoActual
+#                 mesMayor = mesActual
+#
+#             elif montoActual < montoMesMenor:
+#                 montoMesMenor = montoActual
+#                 mesMenor = mesActual
+#
+#         montoActual = monto
+#         mesActual = mesNext
+#
+#
+# if mesMayor and mesMenor:
+#     print("Mes y monto mayor: %s\t\t\t%d\n"
+#           "Mes y monto menor: %s\t\t\t%d" % (mesMayor, montoMesMayor, mesMenor, montoMesMenor))
+
 import sys
 
-mesActual = None
-montoActual = 0
-mesNext = None
-
-mesMayor = None
-montoMesMayor = 0
-mesMenor = None
-montoMesMenor = 0
-flag = False
+mes_actual = None
+mes_mayor = None
+mes_menor = None
+monto_actual = 0
+monto_mayor = 0
+monto_menor = 0
+mes = None
+primero = True
 
 for line in sys.stdin:
-    # Mes con Mayor y Menor Venta
     line = line.strip()
-    mesNext, monto = line.split(" ")
+    mes, monto = line.split(" ")
 
     try:
         monto = int(monto)
     except ValueError:
         continue
 
-    if mesActual == mesNext:
-        montoActual += monto
+    if mes_actual == mes:
+        monto_actual += monto
     else:
-        if mesActual:
-            if not flag:
-                montoMesMenor = montoActual
-                montoMesMayor = montoActual
+        if mes_actual:
+            if primero:
+                monto_mayor = monto_actual
+                monto_menor = monto_actual
+                mes_mayor = mes_actual
+                mes_menor = mes_actual
+                primero = False
+            elif monto_actual > monto_mayor:
+                monto_mayor = monto_actual
+                mes_mayor = mes_actual
+            elif monto_actual < monto_menor:
+                monto_menor = monto_actual
+                mes_menor = mes_actual
 
-                mesMayor = mesActual
-                mesMenor = mesActual
+        mes_actual = mes
+        monto_actual = monto
 
-                flag = True
-            elif montoActual > montoMesMayor:
-                montoMesMayor = montoActual
-                mesMayor = mesActual
-
-            elif montoActual < montoMesMenor:
-                montoMesMenor = montoActual
-                mesMenor = mesActual
-
-        montoActual = monto
-        mesActual = mesNext
-
-
-if mesMayor and mesMenor:
-    print("Mes y monto mayor: %s\t%d\n"
-          "Mes y monto menor: %s\t%d" % (mesMayor, montoMesMayor, mesMenor, montoMesMenor))
+if mes_actual == mes:
+    print
+    "Menor: %s %d\tMayor: %s %d" % (mes_menor, monto_menor, mes_mayor, monto_mayor)
