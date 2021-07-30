9bb62910be2e9b872f6fb3db6bd43b71c23adf6d
Gabriel <20170388@ce.pucmm.edu.do>
2021-07-30 00:12:13 -0400

    test

--- c-reducer.py
+++ c-reducer.py
@@ -1,94 +1,51 @@
 #!/usr/bin/env python
 
-# import sys
-#
-# mesActual = None
-# montoActual = 0
-# mesNext = None
-#
-# mesMayor = None
-# montoMesMayor = 0
-# mesMenor = None
-# montoMesMenor = 0
-# flag = False
-#
-# for line in sys.stdin:
-#     # Mes con Mayor y Menor Venta
-#     line = line.strip()
-#     mesNext, monto = line.split(" ")
-#
-#     try:
-#         monto = int(monto)
-#     except ValueError:
-#         continue
-#
-#     if mesActual == mesNext:
-#         montoActual += monto
-#     else:
-#         if mesActual:
-#             if not flag:
-#                 montoMesMenor = montoActual
-#                 montoMesMayor = montoActual
-#
-#                 mesMayor = mesActual
-#                 mesMenor = mesActual
-#
-#                 flag = True
-#             elif montoActual > montoMesMayor:
-#                 montoMesMayor = montoActual
-#                 mesMayor = mesActual
-#
-#             elif montoActual < montoMesMenor:
-#                 montoMesMenor = montoActual
-#                 mesMenor = mesActual
-#
-#         montoActual = monto
-#         mesActual = mesNext
-#
-#
-# if mesMayor and mesMenor:
-#     print("Mes y monto mayor: %s\t\t\t%d\n"
-#           "Mes y monto menor: %s\t\t\t%d" % (mesMayor, montoMesMayor, mesMenor, montoMesMenor))
-
 import sys
 
-mes_actual = None
-mes_mayor = None
-mes_menor = None
-monto_actual = 0
-monto_mayor = 0
-monto_menor = 0
-mes = None
-primero = True
+mesActual = None
+montoActual = 0
+mesNext = None
+
+mesMayor = None
+montoMesMayor = 0
+mesMenor = None
+montoMesMenor = 0
+flag = False
 
 for line in sys.stdin:
+    # Mes con Mayor y Menor Venta
     line = line.strip()
-    mes, monto = line.split(" ")
+    mesNext, monto = line.split(" ")
 
     try:
         monto = int(monto)
     except ValueError:
         continue
 
-    if mes_actual == mes:
-        monto_actual += monto
+    if mesActual == mesNext:
+        montoActual += monto
     else:
-        if mes_actual:
-            if primero:
-                monto_mayor = monto_actual
-                monto_menor = monto_actual
-                mes_mayor = mes_actual
-                mes_menor = mes_actual
-                primero = False
-            elif monto_actual > monto_mayor:
-                monto_mayor = monto_actual
-                mes_mayor = mes_actual
-            elif monto_actual < monto_menor:
-                monto_menor = monto_actual
-                mes_menor = mes_actual
+        if mesActual:
+            if not flag:
+                montoMesMenor = montoActual
+                montoMesMayor = montoActual
 
-        mes_actual = mes
-        monto_actual = monto
+                mesMayor = mesActual
+                mesMenor = mesActual
 
-if mes_actual == mes:
-    print("Menor: %s %d\tMayor: %s %d" % (mes_menor, monto_menor, mes_mayor, monto_mayor))
+                flag = True
+            if montoActual > montoMesMayor:
+                montoMesMayor = montoActual
+                mesMayor = mesActual
+
+            elif montoActual < montoMesMenor:
+                montoMesMenor = montoActual
+                mesMenor = mesActual
+
+        montoActual = monto
+        mesActual = mesNext
+
+
+if mesMayor and mesMenor:
+    print("Mes y monto mayor: %s\t\t\t%d\n"
+          "Mes y monto menor: %s\t\t\t%d" % (mesMayor, montoMesMayor, mesMenor, montoMesMenor))
--- d-reducer.py
+++ d-reducer.py
@@ -1,76 +1,41 @@
 #!/usr/bin/env python
 
-# import sys
-#
-# ciudadActual = None
-# montoActual = 0
-# ciudadNext = None
-#
-# ciudadMayor = None
-# montociudadMayor = 0
-#
-# flag = False
-#
-# for line in sys.stdin:
-#     # Ciudad con Mayor Venta
-#     line = line.strip()
-#     ciudadNext, monto = line.split(" ")
-#
-#     try:
-#         monto = int(monto)
-#     except ValueError:
-#         continue
-#
-#     if ciudadActual == ciudadNext:
-#         montoActual += monto
-#     else:
-#         if ciudadActual:
-#             if not flag:
-#                 montociudadMayor = montoActual
-#                 ciudadMayor = ciudadActual
-#
-#             elif montoActual > montociudadMayor:
-#                 montociudadMayor = montoActual
-#                 ciudadMayor = ciudadActual
-#
-#         montoActual = monto
-#         ciudadActual = ciudadNext
-#
-#
-# if ciudadMayor:
-#     print("Ciudad de Monto Mayor: %s\t\t\t%d" % (ciudadMayor, montociudadMayor))
 import sys
 
-ciudad_actual = None
-ciudad_mayor = None
-monto_actual = 0
-monto_mayor = 0
-ciudad = None
-primero = True
+ciudadActual = None
+montoActual = 0
+ciudadNext = None
+
+ciudadMayor = None
+montoCiudadMayor = 0
+
+flag = False
 
 for line in sys.stdin:
+    # Ciudad con Mayor Venta
     line = line.strip()
-    ciudad, monto = line.split(" ")
+    ciudadNext, monto = line.split(" ")
 
     try:
         monto = int(monto)
     except ValueError:
         continue
 
-    if ciudad_actual == ciudad:
-        monto_actual += monto
+    if ciudadActual == ciudadNext:
+        montoActual += monto
     else:
-        if ciudad_actual:
-            if primero:
-                monto_mayor = monto_actual
-                ciudad_mayor = ciudad_actual
-            elif monto_actual > monto_mayor:
-                monto_mayor = monto_actual
-                ciudad_mayor = ciudad_actual
-            primero = False
+        if ciudadActual:
+            if not flag:
+                montoCiudadMayor = montoActual
+                ciudadMayor = ciudadActual
 
-        ciudad_actual = ciudad
-        monto_actual = monto
+            if montoActual > montoCiudadMayor:
+                montoCiudadMayor = montoActual
+                ciudadMayor = ciudadActual
 
-if ciudad_actual == ciudad:
-    print("%s %d" % (ciudad_mayor, monto_mayor))
+        montoActual = monto
+        ciudadActual = ciudadNext
+
+
+if ciudadMayor:
+    print("Ciudad de Monto Mayor: %s\t\t\t%d" % (ciudadMayor, montoCiudadMayor))
